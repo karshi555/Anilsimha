1d5a31f6a337d89c0c032a896182e2b5059ec1a4
karshjarak <karshi555@gmail.com>
2021-08-25 12:14:51 +0530

    hey

---
+++ src/test/java/testpackage/test1.java
@@ -0,0 +1,15 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class test1 extends baseclass {
+	
+	
+	@Test
+	public void loginNow() {
+		
+	
+		driver.get("http://facebook.com");
+	}
+
+}
